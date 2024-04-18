b53181bb86dccd70945c170e66d5105e49fe62e0
Johannes Osterkamp <j.osterkamp@lms-sport.de>
2024-04-18 16:33:48 +0200

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+  let name = "Johannes";
+  let age = 29;
+  let warning = null;
+  const increaseAge = () => {
+    age += 1;
+  };
+
+  const decreaseAge = () => {
+    age -= 1;
+  };
+
+  $: uppercase = name.toUpperCase();
+
+  $: if (age > 40) {
+    warning = "Zu alt";
+  } else {
+    warning = "Alles cool";
+  }
+</script>
+
+<main>
+  <h3>Vite + Svelte</h3>
+  <p>{name}</p>
+  <p>{age}</p>
+  <p>{uppercase}</p>
+  <button on:click={increaseAge}>Increase Age</button>
+  <button on:click={decreaseAge}>Decrease Age</button>
+  <p>{warning}</p>
+</main>
